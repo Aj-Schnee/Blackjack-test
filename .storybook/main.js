ecb57ee5af3c60a26537dc5ae1aaa60b581c7bca
Arturo <arturopardo@gmail.com>
2024-12-09 09:58:54 -0800

    storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,15 +4,21 @@ const config = {
     "../stories/**/*.mdx",
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
+
   addons: [
     "@storybook/addon-webpack5-compiler-swc",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/addon-designs",
+    "@storybook/addon-mdx-gfm"
   ],
+
   framework: {
     name: "@storybook/html-webpack5",
     options: {},
   },
+
+  docs: {}
 };
 export default config;
---
+++ stories/newButton.stories.js
@@ -0,0 +1,13 @@
+export default {
+    title: "My stories",
+    component: Button,
+  };
+  
+  export const myStory = {
+    parameters: {
+      design: {
+        type: "figma",
+        url: "https://www.figma.com/design/K6bw18qq5LNNmFloFBZkKc/New-Redesign?node-id=2877-5068&t=Rl1NIe5q1PFbtPvd-1",
+      },
+    },
+  };
